9ea668975d43a65747ef093d1cedca3818bc9482
jose-cleiton <cleitons835@gmail.com>
2022-02-20 14:34:11 -0300

    8. Renderizado o componente Title dentro do componente Missions.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <main>
         <Header />
         <SolarSystem />
+        <Title />
         <Missions />
-      </>
+      </main>
     );
   }
 }
