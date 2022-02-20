1162b367486fc7fdb66b770ee0c1164b9930425a
jose-cleiton <cleitons835@gmail.com>
2022-02-20 12:53:21 -0300

    3. Criado um componente chamado Title e 4. Renderizado o componente Title dentro do componente SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,14 @@
+/** @format */
+
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title key="headline" headline="Planetas" />
+      </div>
     );
   }
 }
