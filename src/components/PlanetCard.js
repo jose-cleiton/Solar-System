601c5db8c6e8207126f2d72339059a23e1320963
jose-cleiton <cleitons835@gmail.com>
2022-02-20 13:46:49 -0300

    5 - Criado um componente chamado PlanetCard

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropType.string,
+  planetImage: PropType.string,
+};
+PlanetCard.defaultProps = {
+  planetName: 'Terra',
+  planetImage: '../images/Terra.png',
+};
+
+export default PlanetCard;
