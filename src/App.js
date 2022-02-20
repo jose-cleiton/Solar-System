dd06ae4fac0f629b733a0f41a41c590710792bd1
jose-cleiton <cleitons835@gmail.com>
2022-02-20 11:51:27 -0300

    1. Criado um componente chamado Header corr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header ';
+import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
